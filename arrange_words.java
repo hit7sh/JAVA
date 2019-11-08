8c8aada85883bfd37cac83c391c74504fdd5be27
Hitesh Kumar Saini <57103779+hit7sh@users.noreply.github.com>
2019-11-08 20:51:51 +0530

    Update arrange_words.java

--- arrange_words.java
+++ arrange_words.java
@@ -9,7 +9,7 @@
 import java.util.Scanner;
 class arrange_words
 {
-    static void main()
+    public static void main(String args[])
     {
         Scanner in=new Scanner(System.in);
         System.out.println("ENTER A SENTENCE");
