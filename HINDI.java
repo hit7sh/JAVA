2d468041df93abe39f4512d1c175058dc28af3a8
Hitesh Kumar Saini <57103779+hit7sh@users.noreply.github.com>
2019-11-18 23:55:59 +0530

    Create HINDI.java

---
+++ HINDI.java
@@ -0,0 +1,36 @@
+class HINDI
+{
+    static void jaihind()
+    {
+        int i,k=0;
+        for(i=2309;i<=2361;i++)
+        {
+            if(k!=10)
+            System.out.print((char)(i)+" ");
+            else
+            {
+                k=0;
+            System.out.println();
+        }
+            k++;
+        }
+        
+       //============================
+       for(i=2365;i<=2381;i++)
+       {
+           if(k!=10)
+            System.out.print((char)(i)+" ");
+            else
+            {
+                k=0;
+            System.out.println();
+        }
+            k++;
+        }
+        //===============================
+        System.out.println((char)(2364));
+        for(i=2406;i<=2416;i++)
+        System.out.println((char)(i));
+    }
+}
+        
